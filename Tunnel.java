07182715bcc3a882c88209ed3f48f2efa41297e7
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-01-06 18:30:53 +0200

    Update Tunnel.java
    в конструктор класса и в метод добавлен семафор

--- Tunnel.java
+++ Tunnel.java
@@ -1,21 +1,31 @@
 package Races;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage{
-    public Tunnel() {
-        this.length = 80;
+
+    private Semaphore entrance; // добавлен семафор
+
+    public Tunnel(Semaphore entrance) {
+        this.length = 150;
         this.description = "Тоннель " + length + " метров";
+        this.entrance = entrance;
+
+
     }
     @Override
-    public void go(Car c) {
+    public void go(Races.Car c) {
         try {
             try {
                 System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
+                this.entrance.acquire(); // добавлена проверка на семафоре
+                System.out.println(c.getName() + " начал этап: " + description + " в " + System.currentTimeMillis());
                 Thread.sleep(length / c.getSpeed() * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                System.out.println(c.getName() + " закончил этап: " + description);
+                System.out.println(c.getName() + " закончил этап: " + description + " в " + System.currentTimeMillis());
+                this.entrance.release();
             }
         } catch (Exception e) {
             e.printStackTrace();
