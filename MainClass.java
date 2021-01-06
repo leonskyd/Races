b858c23a96cfc2814ad531a020ce21c258d064ab
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-01-06 18:32:54 +0200

    Update MainClass.java
    Добавлена реализация Executore Service, Semaphor, CyclicBarier

--- MainClass.java
+++ MainClass.java
@@ -1,19 +1,37 @@
 package Races;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
     public static void main(String[] args) {
+
+        ExecutorService lap = Executors.newFixedThreadPool(CARS_COUNT);
+        CyclicBarrier startline = new CyclicBarrier(CARS_COUNT);
+        Semaphore entrance1 = new Semaphore(CARS_COUNT/2);
+        Finish finish = new Finish(); // Создан класс для финишного этапа, который наследует Stage
+        Race race = new Race(new Road(120), new Tunnel(entrance1), new Road(80),finish);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
-        }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
-        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        for (int i = 0; i < CARS_COUNT; i++) { // используется executor для запуска потоков
+           lap.execute(new Car(race, 20 + (int) (Math.random() * 10), startline));
+        }
+        lap.shutdown();
+
+        try {
+            Thread.sleep(30000); // не нашел на данный момент нормального решения кроме как усыпить на предполагаемое время гонки
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        System.out.println(" Гонка завершилась с таким результатом " + finish.getRaceList().toString()); // Здесь мы получаем результат гонки
     }
 
 }
